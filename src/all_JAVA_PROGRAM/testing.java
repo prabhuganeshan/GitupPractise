18be69bd1814dfedbebe034a17481eebfbf19403
altimetrikprabhuganesan <altimetrikprabhuganesan@gmail.com>
2023-09-08 15:35:05 +0530

    lead commit

---
+++ src/all_JAVA_PROGRAM/testing.java
@@ -0,0 +1,23 @@
+package all_JAVA_PROGRAM;
+
+public class Swapnumberwithout3rdvariable {
+
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+
+        a=(a+b);
+        //System.out.println("a = "+a);
+
+
+        b=(a-b);
+        System.out.println("b = "+b);
+
+        a=a-b;
+        System.out.println("a = "+a);
+
+
+
+    }
+
+}
