964e1b2009b3d2beda5311eba625b0f5cf89b0ff
altimetrikprabhuganesan <altimetrikprabhuganesan@gmail.com>
2023-09-08 17:15:25 +0530

    lead commit

---
+++ src/all_JAVA_PROGRAM/testing.java
@@ -0,0 +1,23 @@
+package all_JAVA_PROGRAM;
+
+public class Amstrong {
+	public static void main(String[] args) {
+		int a=153;
+		int res=0;
+		int rem=0;
+		int num=a;
+		while(a>0) {
+			rem=a%10;
+			res=res+(rem*rem*rem);
+			a=a/10;
+		}
+		System.out.println(res);
+		if(num==res) {
+			System.out.println("amstrong");
+		}
+		else {
+			System.out.println("not amstrang");
+		}
+	}
+
+}
